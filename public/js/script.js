61a54f041a2c7a8c078a1b3e4a5b12ee75435a41
David San Antonio <davsan56@gmail.com>
2017-11-16 20:51:33 -0500

    Added hover changes and optimized updates.

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,8 @@ var socket = io();
 
 // Name of room
 var name;
+var board;
+var color;
 
 // Called when user wants to start a new game
 function newG() {
@@ -11,7 +13,7 @@ function newG() {
 
 // Called when user wants to join a game
 function join() {
- socket.emit('join', {name: $('#joinId').val().toLowerCase()});
+ socket.emit('join', {name: $('#joinId').val().toLocaleLowerCase()});
 }
 
 // Called when user clicks on a column to place piece
@@ -43,60 +45,129 @@ socket.on('new', function(game) {
  $('#board').empty(); // empty the board
  $('#message').empty(); // empty the messages
  name = game.name; // set the name of the game
+ board = game.board; // set the board
  $('#title').text(game.name); // set the room code
- // Make sure there are two players
- if (game.players == 2) {
-   // Let the player know who's turn it is
-   if (game.turn == socket.id)
-     $('#message').html('<h3>It is your turn</h3>');
-   else
-     $('#message').html('<h3>Waiting for other player ...</h3>');
-   // Display the board
+ // Display the board
+ if (game.board) {
    for (var i = 0; i < 6; i++) {
      $('#board').append('\
        <div class="col s4">\
          <div class="row no-bottom-margin">\
            <div class="col s4"></div> <!-- this column empty -->\
-           <div onclick="update(event)" class="col s4 box ' + getColor(game.board[(i * 7) + 0]) +
-             '" id="' + ((i * 7) + 0) + '"></div>\
-           <div onclick="update(event)" class="col s4 box ' + getColor(game.board[(i * 7) + 1]) +
-             '" id="' + ((i * 7) + 1) + '"></div>\
+           <button onclick="update(event)" class="btn col s4 box ' + getColor(game.board[(i * 7) + 0]) +
+             '" id="' + ((i * 7) + 0) + '"></button>\
+           <button onclick="update(event)" class="btn col s4 box ' + getColor(game.board[(i * 7) + 1]) +
+             '" id="' + ((i * 7) + 1) + '"></button>\
          </div>\
        </div>\
        \
        <div class="col s8">\
          <div class="row no-bottom-margin">\
-           <div onclick="update(event)" class="col s2 box ' + getColor(game.board[(i * 7) + 2]) +
-             '" id="' + ((i * 7) + 2) + '"></div>\
-           <div onclick="update(event)" class="col s2 box ' + getColor(game.board[(i * 7) + 3]) +
-             '" id="' + ((i * 7) + 3) + '"></div>\
-           <div onclick="update(event)" class="col s2 box ' + getColor(game.board[(i * 7) + 4]) +
-             '" id="' + ((i * 7) + 4) + '"></div>\
-           <div onclick="update(event)" class="col s2 box ' + getColor(game.board[(i * 7) + 5]) +
-             '" id="' + ((i * 7) + 5) + '"></div>\
-           <div onclick="update(event)" class="col s2 box ' + getColor(game.board[(i * 7) + 6]) +
-             '" id="' + ((i * 7) + 6) + '"></div>\
+           <button onclick="update(event)" class="btn col s2 box ' + getColor(game.board[(i * 7) + 2]) +
+             '" id="' + ((i * 7) + 2) + '"></button>\
+           <button onclick="update(event)" class="btn col s2 box ' + getColor(game.board[(i * 7) + 3]) +
+             '" id="' + ((i * 7) + 3) + '"></button>\
+           <button onclick="update(event)" class="btn col s2 box ' + getColor(game.board[(i * 7) + 4]) +
+             '" id="' + ((i * 7) + 4) + '"></button>\
+           <button onclick="update(event)" class="btn col s2 box ' + getColor(game.board[(i * 7) + 5]) +
+             '" id="' + ((i * 7) + 5) + '"></button>\
+           <button onclick="update(event)" class="btn col s2 box ' + getColor(game.board[(i * 7) + 6]) +
+             '" id="' + ((i * 7) + 6) + '"></button>\
            <div class="col s2"></div> <!-- this column empty -->\
          </div>\
        </div>'
      );
    }
-   // If there is a winner, the game is over and someone has won
-   if (game.winner) {
-     if (game.winner == socket.id)
-       $('#message').html('<h3>You have won!!</h3>');
-     else
-       $('#message').html('<h3>You have lost :(</h3>');
-
-     $('#restartButton').show();
-   }
- } else {
-   // Gets here when there is only one player and shows message
-   $('#message').html(game.message);
-   $('#contain').hide(); // hides the board
  }
+ // Gets here when there is only one player and shows message
+ $('#message').html(game.message);
+ $('#contain').hide(); // hides the board
  if (game.color && $('#color').text() == "") {
    $('#color').html("<h5>Your color is: <span class='" + game.color + "'>" + game.color + "</span></h5>");
+   color = game.color;
+ }
+ if (game.players == 2) {
+   // Show the board
+   $('#contain').show();
+   // Let the player know who's turn it is
+   if (game.turn == socket.id)
+     $('#message').html('<h3>It is your turn</h3>');
+   else
+     $('#message').html('<h3>Waiting for other player ...</h3>');
+ }
+});
+
+// Get every mouseenter and mouseleave event on all the buttons
+$(document).on({
+ // Mouse enter
+ mouseenter: function () {
+   // Get the id
+   var id = $(this).attr('id');
+   // If there is an id add the hover class so the player knows where their piece will go
+   if (id) {
+     var column = id % 7;
+     $('#' + getIndexToHover(column)).addClass(color + "-hover");
+     console.log($('#' + getIndexToHover(column)))
+   }
+ },
+ // Mouse leave
+ mouseleave: function () {
+   // Get the id
+   var id = $(this).attr('id');
+   // If there is an id, remove a hover class
+   if (id) {
+     var column = id % 7;
+     $('#' + getIndexToHover(column)).removeClass(color + "-hover");
+   }
+ }
+}, "button");
+
+// Update the board with the correct color given a column number
+function getIndexToHover(columnNumber) {
+ columnNumber = parseInt(columnNumber);
+ if (board[35 + columnNumber] == '-') {
+   return 35 + columnNumber;
+ } else if (board[28 + columnNumber] == '-') {
+   return 28 + columnNumber;
+ } else if (board[21 + columnNumber] == '-') {
+   return 21 + columnNumber;
+ } else if (board[14 + columnNumber] == '-') {
+   return 14 + columnNumber;
+ } else if (board[7 + columnNumber] == '-') {
+   return 7 + columnNumber;
+ } else if (board[0 + columnNumber] == '-') {
+   return 0 + columnNumber;
+ } else {
+   // Column full
+   return -1;
+ }
+}
+
+// Called when there is an update to the game
+socket.on('update', function(game) {
+ // Let the player know who's turn it is
+ if (game.turn == socket.id)
+   $('#message').html('<h3>It is your turn</h3>');
+ else
+   $('#message').html('<h3>Waiting for other player ...</h3>');
+ // Update the board
+ $('#' + game.index).addClass(getColor(game.color));
+ board[game.index] = getColor(game.color);
+ // Remove any pulsing pieces
+ $('.pulse').each(function(i, obj) {
+   $('#' + obj.id).removeClass("btn btn-floating pulse");
+ });
+ // Make the latest piece pulse
+ $('#' + game.index).addClass("btn btn-floating pulse");
+ // Check for win
+ // If there is a winner, the game is over and someone has won
+ if (game.winner) {
+   if (game.winner == socket.id)
+     $('#message').html('<h3>You have won!!</h3>');
+   else
+     $('#message').html('<h3>You have lost :(</h3>');
+
+   $('#restartButton').show();
  }
 });
 
